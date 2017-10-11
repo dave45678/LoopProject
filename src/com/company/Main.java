95956deef029861884e8c28de6ee9d662023d2ea
Dave Wolf <dave45678@gmail.com>
2017-10-11 13:22:27 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        String var = "String Value";
+        var = "(" + var  + ")";
+
+        System.out.print("(" + var  + ")");
+
+        //Write a method with a while loop to print 1 through n in square brackets.
+
+
+
+    }
+}
